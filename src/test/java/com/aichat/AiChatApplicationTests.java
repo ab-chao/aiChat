b07431b6c1917da3b5da0c520ea0215a588eb0f9
User <624784066@qq.com>
2018-05-29 11:06:53 +0800

    saveMgs增删改查

--- src/test/java/com/aichat/AiChatApplicationTests.java
+++ src/test/java/com/aichat/AiChatApplicationTests.java
@@ -1,16 +1,57 @@
 package com.aichat;
 
+import com.aichat.saveMgs.entity.SaveMgsEntity;
+import com.aichat.saveMgs.service.SaveMgsServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Timestamp;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AiChatApplicationTests {
 
+	@Autowired
+	SaveMgsServiceImpl saveMgsServiceImpl;
+
+	SaveMgsEntity saveMgsEntity = new SaveMgsEntity();
+
+	public void setSaveMgsEntity(){
+		saveMgsEntity.setId(1);
+		java.util.Date d = new java.util.Date();
+
+		Timestamp sqlDate = new Timestamp(d.getTime());
+		saveMgsEntity.setCreation_date(sqlDate);
+		saveMgsEntity.setFnd_id(4);
+		saveMgsEntity.setUser_id(6);
+		saveMgsEntity.setUuid("def");
+		saveMgsEntity.setFile_location("abc");
+	}
+
+//	@Test
+//	public void contextLoads() {
+//		setSaveMgsEntity();
+//		saveMgsServiceImpl.insertSaveMgs(saveMgsEntity);
+//	}
+
+//	@Test
+//	public void contextLoads() {
+//		SaveMgsEntity saveMgsEntity = saveMgsServiceImpl.getSaveMgs(1);
+//		System.out.print(saveMgsEntity.getId());
+//	}
+
+//	@Test
+//	public void contextLoads() {
+//		saveMgsServiceImpl.deleteSaveMgs(1);
+//	}
+
 	@Test
 	public void contextLoads() {
+		setSaveMgsEntity();
+		saveMgsServiceImpl.updateSaveMgs(saveMgsEntity);
 	}
 
 }
